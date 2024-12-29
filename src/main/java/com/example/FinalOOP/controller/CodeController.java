16ee27b63a966a7e5c14b069aed33ea808790349
Arisbek <71332684+Arisbek@users.noreply.github.com>
2024-12-29 19:36:18 +0600

    final

--- src/main/java/com/example/FinalOOP/controller/CodeController.java
+++ src/main/java/com/example/FinalOOP/controller/CodeController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/codes")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://vercel.com/arisbeks-projects/final-web")
 public class CodeController {
 
     @Autowired
